38f528505c57d8529dc491051a52c353cc58f320
Eric Williams <ericlwilliams@gmail.com>
2015-03-19 20:52:58 -0700

    test commit

---
+++ global.R
@@ -0,0 +1,12 @@
+
+## Does the fraction of non-photos in a group effect the communicativity of photoed PPTs
+
+rm(list=ls(all=TRUE)[!(ls(all=TRUE) %in% c("global.vars",global.vars()))])
+#rm(list=ls(all=TRUE)[!(ls(all=TRUE) %in% c("global.vars",global.vars()) | !(ls(all=TRUE) %in% c("global.vars",global.vars())) )])
+assign("last.warning",NULL,envir=baseenv())
+
+
+#ts.data <- data.table(read.csv("./data/datakind_extract_keyfields.csv",stringsAsFactors=FALSE))
+ts.data <- data.table(read.csv("./data/sample_dk_extract.csv",stringsAsFactors=FALSE))
+
+
